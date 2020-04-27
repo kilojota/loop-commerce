dc3eca3c9ce8e429898f71de976e8a7244826d0a
kilojota <fabricioyacques@hotmail.com>
2020-04-27 11:57:00 -0300

    [FO-6] Added products list to home

--- src/actions/productsActions.js
+++ src/actions/productsActions.js
@@ -1,9 +1,14 @@
-import { GET_PRODUCTS } from 'actions/types'
+import { GET_PRODUCTS, CLEAR_PRODUCTS } from 'actions/types'
 import ProductsService from 'api/ProductsService'
 
 export const getProducts = () => async dispatch => {
   try {
     const res = await ProductsService.getProducts()
     dispatch({ type: GET_PRODUCTS, payload: res.data.products })
-  } catch (err) {}
+  } catch (err) {
+
+    dispatch({ type: CLEAR_PRODUCTS, payload: [] })
+
+
+  }
 }
