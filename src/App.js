8124f2260ae43e6eb6b5189aea7c659ab8d569f5
kilojota <fabricioyacques@hotmail.com>
2020-04-27 14:12:41 -0300

    [FO-2] Fixed imports

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
-import React, { useState } from 'react'
-import { IntlProvider } from 'react-intl'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from 'store'
-import flatten from 'flat'
-import ForgotPasswordPage from 'components/pages/ForgotPasswordPage'
-import SignInPage from 'components/pages/SignInPage'
-import SignUpPage from 'components/pages/SignUpPage'
-import ProtectedRoutes from 'components/routes/ProtectedRoutes'
-import Home from 'components/Home'
-import AppLocale from 'languageProvider'
-import './App.scss'
+import React, { useState, Suspense, lazy } from 'react';
+import { IntlProvider } from 'react-intl';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'store';
+import flatten from 'flat';
+
+import ProtectedRoutes from 'components/routes/ProtectedRoutes';
+import AppLocale from 'languageProvider';
+import 'App.scss';
+const ForgotPasswordPage = lazy(() => import('components/pages/ForgotPasswordPage'));
+const SignInPage = lazy(() => import('components/pages/SignInPage'));
+const SignUpPage = lazy(() => import('components/pages/SignUpPage'));
+const Home = lazy(() => import('components/Home'));
 
 const App = () => {
-  const [currentLocale, setCurrentLocale] = useState(navigator.language)
-  const currentAppLocale =
-    currentLocale in AppLocale ? AppLocale[currentLocale] : AppLocale.en
+  const [currentLocale, setCurrentLocale] = useState(navigator.language);
+  const currentAppLocale = currentLocale in AppLocale ? AppLocale[currentLocale] : AppLocale.en;
 
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <IntlProvider
-          locale={currentLocale}
-          messages={flatten(currentAppLocale.messages)}
-        >
+        <IntlProvider locale={currentLocale} messages={flatten(currentAppLocale.messages)}>
           <BrowserRouter>
-            <Switch>
-              <ProtectedRoutes path='/' exact>
-                <Home />
-              </ProtectedRoutes>
-              <Route path='/sign-in'>
-                <SignInPage />
-              </Route>
-              <Route path='/sign-up'>
-                <SignUpPage />
-              </Route>
-              <Route path='/forgot-password'>
-                <ForgotPasswordPage />
-              </Route>
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <ProtectedRoutes path='/' exact>
+                  <Home />
+                </ProtectedRoutes>
+                <Route path='/sign-in'>
+                  <SignInPage />
+                </Route>
+                <Route path='/sign-up'>
+                  <SignUpPage />
+                </Route>
+                <Route path='/forgot-password'>
+                  <ForgotPasswordPage />
+                </Route>
+              </Switch>
+            </Suspense>
           </BrowserRouter>
         </IntlProvider>
       </PersistGate>
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
